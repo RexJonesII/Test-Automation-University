93d5807d227f1ea4dfdcc0a0f9b567d8467406c9
Rex Jones II <Rex.Jones@Test4Success.org>
2019-03-19 23:43:06 -0500

    Add files via upload

---
+++ chp6assertions/HardAssert_SoftAssert.java
@@ -0,0 +1,24 @@
+package com.testautomationu.chp5assertions;
+
+import org.testng.annotations.Test;
+
+public class HardAssert_SoftAssert
+{
+	@Test
+	public void exampleAssertTest ()
+	{
+		System.out.println("1. Open Browser");
+		
+		System.out.println("2. Open Application");
+		
+		System.out.println("3. Sign Into The Application");
+		
+		System.out.println("4. Go To Home Page & Verify Home Page");
+		
+		System.out.println("5. Go To Search Page & Verify Search Page");
+		
+		System.out.println("6. Search For User");
+		
+		System.out.println("7. Sign Out Of The Application");		
+	}
+}
